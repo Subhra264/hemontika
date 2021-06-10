ce27fa356b498076d9d46306d0278f369f433593
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-06-10 13:32:51 +0530

    Add Feed component and move utils/ from components/ to src/

---
+++ frontend/src/components/Feed/Feed.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+export default function Feed(props) {
+    const [poems, setPoems] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        fetch('api/poems')
+            .then(req => (
+                req.json()
+            ))
+            .then(result => {
+                if (result.error) throw new Error(result.error);
+                setPoems([...poems, result]);
+            })
+            .catch(err => {
+                setError(err.message);
+            });
+    }, []);
+
+    return (
+        <div className='poem-container'>
+            {
+                poems? 
+                    poems.map((poem) => (
+                        <div key={poem.id} className='poem'>
+                            <h3>{poem.title}</h3>
+                            <p>{poem.author}</p>
+                            <p>{poem.date}</p>
+                        </div>
+                    ))
+                :
+                    (
+                        error?
+                            <div>
+                                {error}
+                            </div>
+                        :
+                            <div>
+                                Loading...
+                            </div>
+                    )
+            }
+        </div>
+    )
+}
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Form from "../common/Form/Form";
-import authenticate from '../utils/authenticate';
+import authenticate from '../../utils/authenticate';
 
 export default function SignUp(props) {
     const [username, setUsername] = useState('');
