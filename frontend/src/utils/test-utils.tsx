d13c0c35721c5d5356267ccb00311b0c46f6b914
Subhra264 <chakrabortysubhradeep@gmail.com>
2021-09-15 13:30:13 +0530

    Add /utils/test-utils.tsx, update tests for LogIn and SignUp

---
+++ frontend/src/utils/test-utils.tsx
@@ -0,0 +1,28 @@
+import { render, RenderOptions } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+interface RouterWrappedRenderOptions extends RenderOptions {
+    initialEntries?: string[];
+}
+
+interface WrappedWithMemRouterProps extends RouterWrappedRenderOptions{
+    children?: React.ReactNode;
+} 
+
+const WrappedWithMemRouter: React.FC<WrappedWithMemRouterProps> = (props) => {
+    return (
+        <Router initialEntries={props.initialEntries}>
+            {props.children}
+        </Router>
+    );
+};
+
+export function renderWrappedWithRouter (ui: React.ReactElement, options: RouterWrappedRenderOptions) {
+    const initialEntries = options.initialEntries? options.initialEntries : ['/'];
+
+    render(ui, {
+        wrapper: (args) => <WrappedWithMemRouter {...args} initialEntries={initialEntries} />,
+        ...options
+    });
+}
