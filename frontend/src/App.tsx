7cd5c4396b89b5a6de4b19c788ed5cdc02dcce63
Subhra264 <chakrabortysubhradeep@gmail.com>
2021-09-21 22:03:20 +0530

    Add src/components/Cards/, modify css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import LogIn from './components/LogIn/LogIn';
 import SignUp from './components/SignUp/SignUp';
+import Card from './components/Cards/Card';
 
 function App() {
   return (
     <div className="App">
+      <Card />
       <Switch>
         <Route path='/log-in'>
           <LogIn />
