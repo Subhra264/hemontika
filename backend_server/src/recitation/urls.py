68321ef4c02a6c26cae92355195d97d1fe5ba3ee
Abhra303 <chakrabortyabhradeep79@gmail.com>
2021-10-02 20:01:58 +0530

    improve serializers and views and change several endpoints

--- backend_server/src/recitation/urls.py
+++ backend_server/src/recitation/urls.py
@@ -1,6 +1,9 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
 
-router.register(r"^recitations", views.RecitationViewSet, basename="recitation.recitations")
+urlpatterns = [
+    path("list/", views.ListRecitationView.as_view(), name="recitation.recitations"),
+    path("create", views.CreateRecitationView.as_view(), name="recitation.create"),
+    path("recitation/<int:pk>/", views.RetrieveUpdateDeleteRecitationView.as_view(), name="recitation.recitations_update_delete")
+]
--- backend_server/src/recitation/views.py
+++ backend_server/src/recitation/views.py
@@ -1,9 +1,44 @@
+from rest_framework import status
+from rest_framework.exceptions import NotAcceptable
+from rest_framework.response import Response
 from .models import Recitation
-from .serializers import RecitationSerializer
-from rest_framework.viewsets import ModelViewSet
+from .serializers import CreateDestroyRecitationSerializer, ListRecitationSerializer, RetrieveRecitationSerializer, UpdateRecitationSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 
 
-class RecitationViewSet(ModelViewSet):
+class ListRecitationView(ListAPIView):
     queryset = Recitation.objects.all()
-    serializer_class = RecitationSerializer
-    http_method_names = ["get", "post", "patch", "delete", "head"]
+    serializer_class = ListRecitationSerializer
+
+
+class CreateRecitationView(CreateAPIView):
+    queryset = Recitation.objects.all()
+    serializer_class = CreateDestroyRecitationSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(reciter=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = self.perform_create(serializer)
+        serialized_instance = RetrieveRecitationSerializer(instance)
+        return Response(serialized_instance.data)
+
+
+class RetrieveUpdateDeleteRecitationView(RetrieveUpdateDestroyAPIView):
+    queryset = Recitation.objects.all()
+    serializer_class = RetrieveRecitationSerializer
+
+    def get_serializer_class(self):
+        print("self.request.method ", self.request.method)
+        if self.request.method == "GET":
+            return self.serializer_class
+        elif self.request.method == "PATCH" or self.request.method == "DELETE":
+            return UpdateRecitationSerializer
+        elif self.request.method == "PUT":
+            raise NotAcceptable("The request method is not acceptable")
+
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response({"message": "Recitation object deleted successfully"}, status=status.HTTP_200_OK)
