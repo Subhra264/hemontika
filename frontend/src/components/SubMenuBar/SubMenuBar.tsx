d92bcf06f4a7bb5bbad899a4d22b19b58cb21635
Subhra264 <chakrabortysubhradeep@gmail.com>
2021-10-18 22:48:17 +0530

    Highlight active sub-menu-item in SubMenuBar

--- frontend/src/components/SubMenuBar/SubMenuBar.tsx
+++ frontend/src/components/SubMenuBar/SubMenuBar.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as WriteIcon } from '../../assets/imgs/icon_write.svg';
 import { ReactComponent as ExploreIcon } from '../../assets/imgs/icon_explore.svg';
 import { ReactComponent as HomeIcon } from '../../assets/imgs/icon_home.svg';
@@ -13,13 +13,14 @@ interface SubMenuItemProps {
     icon: JSX.Element
     label: string;
     linkTo: string;
+    active: boolean;
 }
 
 const SubMenuItem: React.FC<SubMenuItemProps> = (props) => {
     const { isMobile } = useViewport();
 
     return (
-        <div className="sub-menu-item" title={props.label}>
+        <div className={`sub-menu-item ${props.active? 'active' : ''}`} title={props.label}>
             <Link to={props.linkTo}>
                 <div className="sub-menu-item-icon">
                     {props.icon}
@@ -37,6 +38,8 @@ const SubMenuItem: React.FC<SubMenuItemProps> = (props) => {
 
 const SubMenuBar: React.FC = (props) => {
     const [showItems, setShowItems] = useState(false);
+    const [activeLinkIndex, setActiveLinkIndex] = useState(2);
+    const location = useLocation();
     const menuItems = useRef([
         { linkTo: '/write', label: 'Write', icon: <WriteIcon /> },
         { linkTo: '/explore', label: 'Explore', icon: <ExploreIcon /> },
@@ -44,6 +47,31 @@ const SubMenuBar: React.FC = (props) => {
         { linkTo: '/popular', label: 'Popular', icon: <PopularIcon /> },
         { linkTo: '/profile', label: 'Profile', icon: <ProfileIcon /> }
     ]);
+
+    const getCurrRouteIndex = (): number => {
+        let currentRouteIndex = 2;
+        console.log('Location', location);
+
+        for (let i = 0; i < menuItems.current.length; i++) {
+            const linkTo = menuItems.current[i].linkTo;
+            console.log('getCurrRouteIndex linkTo', linkTo);
+            console.log('getCurrRouteIndex location.pathname', location.pathname);
+            if (linkTo === location.pathname
+                || (linkTo !== '/'
+                    && location.pathname.startsWith(linkTo)
+                )) {
+                currentRouteIndex = i;
+                break;
+            }
+        }
+        console.log('getCurrRouteIndex current index', currentRouteIndex);
+        return currentRouteIndex;
+    };
+
+    useEffect(() => {
+        console.log('Inside useEffect SubMenuBar');
+        setActiveLinkIndex(getCurrRouteIndex());
+    }, [location]);
 
     const switchMenuItems: React.MouseEventHandler = (ev: React.MouseEvent) => {
         setShowItems(showItems => !showItems);
@@ -56,9 +84,13 @@ const SubMenuBar: React.FC = (props) => {
             </div>
             <div className={`sub-menu-items ${showItems? 'show-sub-menu-items' : ''}`}>
                 {
-                    menuItems.current.map((menuItem) => (
-                        <SubMenuItem {...menuItem} key={menuItem.label}/>
-                    ))
+                    menuItems.current.map((menuItem, index) => {
+                        let active = false;
+                        if (index === activeLinkIndex) {
+                            active = true;
+                        }
+                        return <SubMenuItem {...menuItem} active={active} key={menuItem.label}/>
+                    })
                 }
             </div>
         </div>
